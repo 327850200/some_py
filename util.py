4a29f32d2e3c9bb7978a3708e898f2b71987d539
327850200 <327850200@qq.com>
2017-06-15 10:38:36 +0800

    Create util.py

---
+++ util.py
@@ -0,0 +1,19 @@
+def stock_code_to_wind_code(stock):
+    inner_stock = str(stock)
+    orig_len = len(inner_stock)
+    if orig_len < 6:
+       inner_stock = '0'*(6-orig_len) + inner_stock
+
+
+    if inner_stock[0] == '6':
+        return inner_stock[0:6] + '.SH'
+    elif inner_stock[0] == '1': #深圳分级基金
+        return inner_stock[0:6] + '.SZ'
+    elif inner_stock[0] == '5': #上海分级基金
+        return inner_stock[0:6] + '.SH'
+    elif inner_stock[0] == '0': #深圳主板或中小板
+        return inner_stock[0:6] + '.SZ'
+    elif inner_stock[0] == '3': # 创业板
+        return inner_stock[0:6] + '.SZ'
+    else:
+        return 'ERROR.STOCK'
